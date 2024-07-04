53f3ad6e9901fc42de61683b45eefa13c648707b
Mars Notahacker <marsXrobertson@gmail.com>
2024-07-04 13:18:50 -0300

    Adding link + reducing signature in lenght

--- src/SupabaseQuery.js
+++ src/SupabaseQuery.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
@@ -57,7 +57,9 @@ const SupabaseQuery = () => {
     }
   };
 
-  executeQuery();
+  useEffect(() => {
+    executeQuery(query);
+  }, []); // Empty dependency array ensures this runs only once on initial load
 
   return (
     <div>
@@ -94,18 +96,30 @@ const SupabaseQuery = () => {
                   <td>
                     <ol>
                       {result.most_viewed.map((article, i) => (
-                        <li key={i}>{article}</li>
+                        <li key={i}>{article}
+                          { result.most_viewed_link && result.most_viewed_link[i] &&
+                            <a href={result.most_viewed_link[i]}>
+                            <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="Interface / External_Link"><path id="Vector" d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></g></svg>
+                            </a>
+                          }
+                        </li>
                       ))}
                     </ol>
                   </td>
                   <td>
                     <ol>
                       {result.most_read.map((article, i) => (
-                        <li key={i}>{article}</li>
+                        <li key={i}>{article}
+                          { result.most_read_link && result.most_read_link[i] &&
+                            <a href={result.most_read_link[i]}>
+                            <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="Interface / External_Link"><path id="Vector" d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></g></svg>
+                            </a>
+                          }
+                        </li>
                       ))}
                     </ol>
                   </td>
-                  <td>{result.signature}</td>
+                  <td>{result.signature.length > 400 ? result.signature.substring(0, 400) + '...' : result.signature}</td>
                   <td>{new Date(result.timestamp).toLocaleString()}</td>
                 </tr>
               ))}
